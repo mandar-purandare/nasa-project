b6043a6cbd92c115860bfa6428c1dd8ecd83b6e7
Mandar Purandare <mnpsstack@gmail.com>
2023-05-14 12:46:33 +0530

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,11 +3,13 @@ const app = require('../../app');
 const { 
     mongoConnect,
     mongoDisconnect, } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API',() => {
 
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
